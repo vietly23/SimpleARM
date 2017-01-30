d02ab26c782f21607a210644802d0d1bf5066170
Viet Ly <viettl@uci.edu>
2017-01-30 14:53:54 -0800

    Fix combinational logic

--- design/alu.sv
+++ design/alu.sv
@@ -12,44 +12,53 @@ module alu (input logic [31:0] a, b,
 `define ORR 2'b11
 
 logic [1:0] is_overflow;
+logic [31:0] temp;
 
-case(opcode)
-	`ADD:
-	begin
-		assign {carry,c} = a + b;
-		if (a[31] & b[31] & ~c[31])
-			overflow <= 1'b1;
-		else if (~a[31] & ~b[31] & c[31])
-			overflow <= 1'b1;
-		else 
+always_comb
+	case(opcode)
+		`ADD:
+		begin
+			{carry,c} <= a + b;
+			if (a[31] & b[31] & ~c[31])
+				overflow <= 1'b1;
+			else if (~a[31] & ~b[31] & c[31])
+				overflow <= 1'b1;
+			else 
+				overflow <= 1'b0;
+		end
+
+		`SUB:
+		begin
+			{carry,c} <= (a + (~b)) + 1;
+			if (a[31] & ~b[31] & ~c[31])
+				overflow <= 1'b1;
+			else if (~a[31] & b[31] & c[31])
+				overflow <= 1'b1;
+			else 
+				overflow <= 1'b0;
+		end
+
+		`AND:
+		begin
+			c <= a & b;
+			carry <= 1'b0;
 			overflow <= 1'b0;
-	end
-	`SUB:
-	begin
-		assign {carry,c} = (a + (~b)) + 1;
-		if (a[31] & ~b[31] & ~c[31])
-			overflow <= 1'b1;
-		else if (~a[31] & b[31] & c[31])
-			overflow <= 1'b1;
-		else 
+		end
+
+		`ORR:
+		begin
+			c <= a | b;
+			carry <= 1'b0;
 			overflow <= 1'b0;
-	end
-	`ORR:
-		assign c = a | b;
-		carry <= 1'b0;
-		overflow <= 1'b0;
-	`AND:
-		assign c = a & b;
-		carry <= 1'b0;
-		overflow <= 1'b0;
-endcase
+		end
+	endcase
 
-if(c == 0)
-	zero <= 1'b1;
-else
-	zero <= 1'b0;
+always_comb
+	if(temp == 0)
+		zero <= 1'b1;
+	else
+		zero <= 1'b0;
 
-negative <= c[31];
-
-
-end module
+always_comb
+	negative <= temp[31];
+endmodule
