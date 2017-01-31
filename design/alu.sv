7122a3a0d256c47932bb6a987b38892233f9ece5
Viet Ly <viettl@uci.edu>
2017-01-30 16:11:23 -0800

    Consolidate flags into array

--- design/alu.sv
+++ design/alu.sv
@@ -1,17 +1,15 @@
 module alu (input logic [31:0] a, b,
 	input logic [1:0] opcode,
 	output logic [31:0] c,
-	output logic zero,
-	output logic negative,
-	output logic carry,
-	output logic overflow);
+	output logic [3:0] flags);
 
 `define ADD 2'b00
 `define SUB 2'b01
 `define AND 2'b10
 `define ORR 2'b11
 
-logic [1:0] is_overflow;
+// From 0 to 3, negative, zero, carry, overflow in that respective order
+
 logic [31:0] temp;
 
 
@@ -19,49 +17,49 @@ always_comb
 	case(opcode)
 		`ADD:
 		begin
-			{carry,temp} <= a + b;
+			{flags[2],temp} <= a + b;
 			if (a[31] & b[31] & ~c[31])
-				overflow <= 1'b1;
+				flags[3] <= 1'b1;
 			else if (~a[31] & ~b[31] & c[31])
-				overflow <= 1'b1;
+				flags[3] <= 1'b1;
 			else 
-				overflow <= 1'b0;
+				flags[3] <= 1'b0;
 		end
 
 		`SUB:
 		begin
-			{carry,temp} <= (a + (~b)) + 1;
+			{flags[2],temp} <= (a + (~b)) + 1;
 			if (a[31] & ~b[31] & ~c[31])
-				overflow <= 1'b1;
+				flags[3] <= 1'b1;
 			else if (~a[31] & b[31] & c[31])
-				overflow <= 1'b1;
+				flags[3] <= 1'b1;
 			else 
-				overflow <= 1'b0;
+				flags[3] <= 1'b0;
 		end
 
 		`AND:
 		begin
 			temp <= a & b;
-			carry <= 1'b0;
-			overflow <= 1'b0;
+			flags[2] <= 1'b0;
+			flags[3] <= 1'b0;
 		end
 
 		`ORR:
 		begin
 			temp <= a | b;
-			carry <= 1'b0;
-			overflow <= 1'b0;
+			flags[2] <= 1'b0;
+			flags[3] <= 1'b0;
 		end
 	endcase
 
 always_comb
 	if(temp == 0)
-		zero <= 1'b1;
+		flags[1] <= 1'b1;
 	else
-		zero <= 1'b0;
+		flags[1] <= 1'b0;
 
 always_comb
-	negative <= temp[31];
+	flags[0] <= temp[31];
 always_comb
 	c <= temp;
 endmodule
