81f84f1ce4cc6051cc94e044c72ad3fffb7bde0d
Nikita Tsvetkov <Nikita Tsvetkov>
2017-02-03 19:13:42 -0800

    change flag indices to macros

--- design/alu.sv
+++ design/alu.sv
@@ -9,6 +9,11 @@ module alu (input logic [31:0] a, b,
 `define ORR 2'b11
 
 // From 0 to 3, negative, zero, carry, overflow in that respective order
+`define NEG 0
+`define ZER 1
+`define CAR 2
+`define OVR 3
+
 
 logic [31:0] temp;
 
@@ -17,49 +22,49 @@ always_comb
 	case(opcode)
 		`ADD:
 		begin
-			{flags[2],temp} <= a + b;
+			{flags[CAR],temp} <= a + b;
 			if (a[31] & b[31] & ~c[31])
-				flags[3] <= 1'b1;
+				flags[OVR] <= 1'b1;
 			else if (~a[31] & ~b[31] & c[31])
-				flags[3] <= 1'b1;
+				flags[OVR] <= 1'b1;
 			else 
-				flags[3] <= 1'b0;
+				flags[OVR] <= 1'b0;
 		end
 
 		`SUB:
 		begin
-			{flags[2],temp} <= (a + (~b)) + 1;
+			{flags[CAR],temp} <= (a + (~b)) + 1;
 			if (a[31] & ~b[31] & ~c[31])
-				flags[3] <= 1'b1;
+				flags[OVR] <= 1'b1;
 			else if (~a[31] & b[31] & c[31])
-				flags[3] <= 1'b1;
+				flags[OVR] <= 1'b1;
 			else 
-				flags[3] <= 1'b0;
+				flags[OVR] <= 1'b0;
 		end
 
 		`AND:
 		begin
 			temp <= a & b;
-			flags[2] <= 1'b0;
-			flags[3] <= 1'b0;
+			flags[CAR] <= 1'b0;
+			flags[OVR] <= 1'b0;
 		end
 
 		`ORR:
 		begin
 			temp <= a | b;
-			flags[2] <= 1'b0;
-			flags[3] <= 1'b0;
+			flags[CAR] <= 1'b0;
+			flags[OVR] <= 1'b0;
 		end
 	endcase
 
 always_comb
 	if(temp == 0)
-		flags[1] <= 1'b1;
+		flags[ZER] <= 1'b1;
 	else
-		flags[1] <= 1'b0;
+		flags[ZER] <= 1'b0;
 
 always_comb
-	flags[0] <= temp[31];
+	flags[NEG] <= temp[31];
 always_comb
 	c <= temp;
 endmodule
