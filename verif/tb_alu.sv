d5fb134b44596fce8edc2e020e3dba78d8e3b27d
Hoàng Kim <Hong Kim>
2017-02-05 17:30:55 +0700

    Add to tb_alu.sv

--- verif/tb_alu.sv
+++ verif/tb_alu.sv
@@ -9,17 +9,41 @@ module tb_alu();
 	logic overflow;
 
 	alu my_alu(a,b,opcode,out,zero,negative,carry,overflow);
+	
+	
 	initial begin
 		a = 32'h0000000A;
 		b = 32'h00000003;
 		opcode = 2'b00;
+		
+		
+		
+		
 	end
 	always @* begin
 	if (out === 13 & zero === 0 & negative === 0 & carry === 0 & overflow === 0)
-		$display("WOW");
+		$display("PASS");
 	else
 		$display("FAIL");
 	end
+	
+	alu_my_alu(a,b,opcode2,out,zero, negative, carry, overflow);
+	initial begin
+		a = 32'h0000000A;
+		b = 32'h00000003;
+		opcode2 = 2'b01;
+		
+		
+		
+		
+	end
+	always @* begin
+	if (out === 7 & zero === 0 & negative === 0 & carry === 0 & overflow === 0)
+		$display("PASS");
+	else
+		$display("FAIL");
+	end
+	
 endmodule
 	
 	
