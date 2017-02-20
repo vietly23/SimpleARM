e34ad4763f24400bf9dc72e999438a64d0e4be90
Viet Ly <viettl@uci.edu>
2017-02-20 11:23:38 -0800

    hook up shifter

--- design/datapath.sv
+++ design/datapath.sv
@@ -16,6 +16,23 @@ module datapath(input logic clk, reset,
 	logic [31:0] PCNext, PCPlus4, PCPlus8; 
 	logic [31:0] ExtImm, SrcA, SrcB, Result; 
 	logic [3:0] RA1, RA2;
+
+	// NEEDS TO BE HOOKED UP
+	// --------------------------------
+	logic [3:0] RA3;
+	logic [31:0] RD3;
+	logic shift_choice;
+	logic [2:0] shift_code;
+	logic [4:0] controller_shift;
+	logic [4:0] shift_amt;
+	logic [31:0] shift_out;
+
+	mux #5 shift_mux(RD3[4:0], controller_shift, shift_choice, shift_amt)
+`
+	// TAKES Result from REGISTERFILE and shifts it
+	shifter m_shift(Result, shift_code, shift_amt, shift_out);
+	// ----------------------------------
+
 	// next PC logic 
 	mux #(32) pcmux(PCPlus4, Result, PCSrc, PCNext); 
 	flopr #(32) pcreg(clk, reset, PCNext, PC); 
@@ -25,12 +42,16 @@ module datapath(input logic clk, reset,
 	// register file logic 
 	mux #(4) ra1mux(Instr[19:16], 4'b1111, RegSrc[0], RA1); 
 	mux #(4) ra2mux(Instr[3:0], Instr[15:12], RegSrc[1], RA2); 
-	regfile rf(clk, RegWrite, RA1, RA2, 
+
+	// RA3 is NOT hooked up -- IMPORTANT
+	regfile rf(clk, RegWrite, RA1, RA2, RA3 
 		Instr[15:12], Result, PCPlus8, 
-		SrcA, WriteData); 
+		SrcA, WriteData, RD3); 
+
 	mux #(32) resmux(ALUResult, ReadData, MemtoReg, Result); 
 	extend ext(Instr[23:0], ImmSrc, ExtImm);
 	// ALU logic 
-	mux #(32) srcbmux(WriteData, ExtImm, ALUSrc, SrcB); 
+	// CHANGED FOR SHIFTER MUX -- IMPORTANT
+	mux #(32) srcbmux(shift_out, ExtImm, ALUSrc, SrcB); 
 	alu alu(SrcA, SrcB, storedCarry, ALUControl, ALUResult, ALUFlags); 
 endmodule
--- design/registerfile.sv
+++ design/registerfile.sv
@@ -1,6 +1,8 @@
 module regfile(input logic clk, 
 		input logic we3, 
-		input logic [3:0] ra1, ra2, ra3, wa3, 
+		input logic [3:0] ra1, ra2,
+		input logic[3:0] ra3,
+		input logic [3:0] wa3, 
 		input logic [31:0] wd3, r15, 
 		output logic [31:0] rd1, rd2, rd3);
 
