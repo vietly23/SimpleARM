2ab34f11af22d5ba3d5ce4ad106f78032382db33
Viet Ly <viettl@uci.edu>
2017-02-20 10:34:06 -0800

    Change registerfile

--- design/registerfile.sv
+++ design/registerfile.sv
@@ -1,8 +1,8 @@
 module regfile(input logic clk, 
 		input logic we3, 
-		input logic [3:0] ra1, ra2, wa3, 
+		input logic [3:0] ra1, ra2, ra3, wa3, 
 		input logic [31:0] wd3, r15, 
-		output logic [31:0] rd1, rd2);
+		output logic [31:0] rd1, rd2, rd3);
 
 	logic [31:0] rf[14:0];
 
@@ -14,4 +14,5 @@ module regfile(input logic clk,
 		if (we3) rf[wa3] <= wd3;
 	assign rd1 = (ra1 == 4'b1111) ? r15 : rf[ra1]; 
 	assign rd2 = (ra2 == 4'b1111) ? r15 : rf[ra2]; 
+	assign rd3 = (ra3 == 4'b1111) ? r15 : rf[ra3]; 
 endmodule
