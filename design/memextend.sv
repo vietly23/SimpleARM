04bf3dea2a6652f33d7f96ae91911cdfce2e32b9
Anthony <adelaro1@uci.edu>
2017-02-26 01:56:24 -0800

    memory extender

--- design/memextend.sv
+++ design/memextend.sv
@@ -1,15 +1,43 @@
-module memextend(input logic [23:0] Instr,
-				input logic [1:0] ImmSrc,
-				output logic [31:0] ExtImm);
+module memextend(input logic [31:0] data,
+                 input logic [2:0] memSelect,//memSelect[2] = loadSigned
+                 input logic [3:0] be,
+                 output logic [31:0] dataOut);
 
+  //codes for memSelect[1:0]
+  `define BYTE 2'h0
+	`define HALF 2'h1
+	`define WORD 2'h2
+  
 	always_comb
-		case(ImmSrc)
+    case(memSelect[1:0])
 				// 8-bit unsigned immediate
-		2'b00: ExtImm = {24'b0, Instr[7:0]};
-				// 12-bit unsigned immediate
-		2'b01: ExtImm = {20'b0, Instr[11:0]};
-				// 24-bit two's complement shifted branch
-		2'b10: ExtImm = {{6{Instr[23]}}, Instr[23:0], 2'b00};
-		default: ExtImm = 32'bx; // undefined
+		`BYTE:begin
+      			if(memSelect[2]) begin
+              if(be[0]) dataOut = {{24{data[7]}}, data[7:0]};
+      				else if(be[1]) dataOut = {{24{data[15]}}, data[15:8]};
+      				else if(be[2]) dataOut = {{24{data[23]}}, data[23:16]};
+              else if(be[3]) dataOut = {{24{data[31]}}, data[31:24]};
+      	end
+      			else begin
+              if(be[0]) dataOut = {24'b0, data[7:0]};
+              else if(be[1]) dataOut = {24'b0, data[15:8]};
+              else if(be[2]) dataOut = {24'b0, data[23:16]};
+              else if(be[3]) dataOut = {24'b0, data[31:24]};
+            end
+      end
+		`HALF:begin
+      			if(memSelect[2]) begin
+              if(be[1:0]) dataOut = {{16{data[15]}}, data[15:0]};
+              else if(be[3:2]) dataOut = {{16{data[31]}}, data[31:16]};
+      	end
+      			else begin
+              if(be[1:0]) dataOut = {16'b0, data[15:0]};
+              else if(be[3:2]) dataOut = {16'b0, data[31:16]};
+            end 
+      end
+		default:begin
+      	dataOut = data[31:0];
+      
+    			end
 	endcase
 endmodule
